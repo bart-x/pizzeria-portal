ce1688eb5bb5bccf2bdb2e98c015c417a5ff41ad
Bartek K <kontener00@gmail.com>
2020-10-05 18:13:31 +0200

    Add view components

---
+++ src/components/views/Kitchen/Kitchen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import styles from './Kitchen.module.scss';
+
+const Kitchen = () => (
+  <div className={styles.component}>
+    <h2>Kitchen views</h2>
+  </div>
+);
+
+Kitchen.propTypes = {
+};
+
+export default Kitchen;
