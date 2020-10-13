ef1d0d14287aa74c8b9e55e04cde8790ffe31bb0
Bartek K <kontener00@gmail.com>
2020-10-13 18:56:58 +0200

    Add new views in the portal app

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import styles from './PageNav.module.scss';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
-const PageNav = (props) => (
-  <nav>
-    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeclassName='active'>Home</Button>
-    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeclassName='active'>Login</Button>
-    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeclassName='active'>Tables</Button>
-    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeclassName='active'>Waiter</Button>
-    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeclassName='active'>Kitchen</Button>
+const PageNav = () => (
+  /* Użyliśmy komponentu NavLink, aby linki dostawały klasę active kiedy jesteśmy stronie danego widoku. */
+  <nav className={styles.component}>
+    <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</Button>
+    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Button>
+    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Button>
+    <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
   </nav>
 );
 
-PageNav.propTypes = {
-  children: PropTypes.node,
-};
+// PageNav.propTypes = {
+// };
 
 export default PageNav;
